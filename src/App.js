76cfed696dc36a9c33788c4ab05173065d4f9d51
Mohan chandra sharma <120721761+Mohan-Chandra-Sharma1@users.noreply.github.com>
2023-01-11 17:43:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
+// export default App;
+import Calculator from "./components/Calculator";
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
