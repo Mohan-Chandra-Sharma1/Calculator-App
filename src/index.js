ccf6f161ca60cb806ac101ac5bb61983f112a162
Mohan chandra sharma <mohan936855@gmail.com>
2023-01-18 17:54:50 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,10 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALap0aibRwHSvDroeTLmgQO0xKAhZAeIs",
+  authDomain: "cart-abc9d.firebaseapp.com",
+  projectId: "cart-abc9d",
+  storageBucket: "cart-abc9d.appspot.com",
+  messagingSenderId: "1011433903236",
+  appId: "1:1011433903236:web:27051e900af826be3d1ed1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
+
+
+
+
